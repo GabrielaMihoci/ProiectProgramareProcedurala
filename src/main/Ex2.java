ffbc5cd6d43e546471176437fff642f32f6dbac8
GabrielaM <Marco@DESKTOP-VKCQ5GH>
2020-02-02 21:46:25 +0200

    Al doilea commit

---
+++ src/main/Ex2.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		double d = 2.95;
+		int i = 4;
+		System.out.println(++d>i?d:i);
+	}
+}
+// afiseaza 4.0 // fara eroare
---
+++ src/main/Ex4.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Ex4 {
+
+	public static void main(String[] args) {
+		int suma = 0;
+		for(int i = 1; i< 10; i++) {
+			suma += suma + i;
+		}
+System.out.print("Suma este: " + suma);
+	}
+}
